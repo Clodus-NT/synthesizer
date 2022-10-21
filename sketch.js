263e352b7c52de092d7faa2914b8609cb1dec25d
Clodus-NT <clodus.nt@gmail.com>
2022-10-21 15:59:04 -0400

    notes are mapped to keyboardOptions and no note will play if the key is not included in options array

--- sketch.js
+++ sketch.js
@@ -9,14 +9,16 @@ const keyOffset = 50;
 let osc, env;
 // The keys on a computer keyboard that will play a note
 const keyboardOptions = [
-  'A', 'W', 'S', 'E', 'D', 'F', 'T', 'G', 'Y', 
-  'H', 'U', 'J', 'K', 'O', 'L', 'P', ';', '\''
+  // 'A', 'W', 'S', 'E', 'D', 'F', 'T', 'G', 'Y', 
+  // 'H', 'U', 'J', 'K', 'O', 'L', 'P', ';', '\''
+  'a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 
+  'h', 'u', 'j', 'k', 'o', 'l', 'p', ';', '\''
 ];
 console.log(keyboardOptions.length)
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  env = new p5.Envelope(0.01, 0.1, 1, 0.5);
+  env = new p5.Envelope(0.01, 0.5, 1, 0.5);
   osc = new p5.Oscillator('square');
   osc.start();
   osc.amp(env);
@@ -31,11 +33,19 @@ function draw() {
   for (let i = 0; i < keyboardOptions.length; i++) {
     fill(255);
     rect(25 + (keyOffset * i), 100, 40, 200);
+    // if (keyIsPressed && key === keyboardOptions[i]) {
+    //   osc.freq(midiToFreq(midiValArr[i+24]))
+    // }
   }
 }
 
 function keyPressed() {
-  env.play();
+  for (let i = 0; i < keyboardOptions.length; i++) {
+    if (keyIsPressed && key === keyboardOptions[i]) {
+      osc.freq(midiToFreq(midiValArr[i+24]));
+      env.play();
+    }
+  }
 }
 
 function windowResized() {
